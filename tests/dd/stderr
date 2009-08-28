m-svo/coreutils

commit 381e69eace06d863a3dcd9d9f3a8f61fb449be0b

dd: avoid unnecessary, but harmless close_stdout call.
* src/dd.c (close_stdout_required): New global.
(maybe_close_stdout): New function.
(main): Set the global.
Reported by Ulrich Drepper in http://bugzilla.redhat.com/436368

src/dd.c
@@ -392,6 +392,25 @@ static char const ebcdic_to_ascii[] =
   '\070', '\071', '\372', '\373', '\374', '\375', '\376', '\377'
 };
 
+/* True if we need to close the standard output *stream*.  */
+static bool close_stdout_required = true;
+
+/* The only reason to close the standard output *stream* is if
+   parse_long_options fails (as it does for --help or --version).
+   In any other case, dd uses only the STDOUT_FILENO file descriptor,
+   and the "cleanup" function calls "close (STDOUT_FILENO)".
+   Closing the file descriptor and then letting the usual atexit-run
+   close_stdout function call "fclose (stdout)" would result in a
+   harmless failure of the close syscall (with errno EBADF).
+   This function serves solely to avoid the unnecessary close_stdout
+   call, once parse_long_options has succeeded.  */
+static void
+maybe_close_stdout (void)
+{
+  if (close_stdout_required)
+    close_stdout ();
+}
+
 void
 usage (int status)
 {
@@ -1655,12 +1674,14 @@ main (int argc, char **argv)
   textdomain (PACKAGE);
 
   /* Arrange to close stdout if parse_long_options exits.  */
-  atexit (close_stdout);
+  atexit (maybe_close_stdout);
 
   page_size = getpagesize ();
 
   parse_long_options (argc, argv, PROGRAM_NAME, PACKAGE, VERSION,
 		      usage, AUTHORS, (char const *) NULL);
+  close_stdout_required = false;
+
   if (getopt_long (argc, argv, "", NULL, NULL) != -1)
     usage (EXIT_FAILURE);
 

commit 1742a31f4dc4948c26310b55b159f7837b4a3f3b

dd: detect closed stderr
* src/dd.c (maybe_close_stdout): Always flush stderr; regression
introduced in commit 381e69ea.
* tests/misc/close-stdout (p): Use consistent style.
* tests/dd/stderr: New test, borrowing from misc/close-stdout.
* tests/Makefile.am (TESTS): Run it.
* NEWS: Mention this.

src/dd.c
@@ -25,6 +25,7 @@
 #include <getopt.h>
 
 #include "system.h"
+#include "close-stream.h"
 #include "error.h"
 #include "fd-reopen.h"
 #include "gethrxtime.h"
@@ -444,12 +445,16 @@ static bool close_stdout_required = true;
    close_stdout function call "fclose (stdout)" would result in a
    harmless failure of the close syscall (with errno EBADF).
    This function serves solely to avoid the unnecessary close_stdout
-   call, once parse_long_options has succeeded.  */
+   call, once parse_long_options has succeeded.
+   Meanwhile, we guarantee that the standard error stream is flushed,
+   by inlining the last half of close_stdout as needed.  */
 static void
 maybe_close_stdout (void)
 {
   if (close_stdout_required)
     close_stdout ();
+  else if (close_stream (stderr) != 0)
+    _exit (EXIT_FAILURE);
 }
 
 void

tests/Makefile.am
@@ -305,6 +305,7 @@ TESTS =						\
   dd/skip-seek					\
   dd/skip-seek2					\
   dd/skip-seek-past-file			\
+  dd/stderr					\
   dd/unblock-sync				\
   df/total-verify				\
   du/2g						\

tests/dd/stderr
@@ -1,0 +1,48 @@
+#!/bin/sh
+# Ensure dd recognizes failure to write to stderr.
+
+# Copyright (C) 2009 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if test "$VERBOSE" = yes; then
+  set -x
+  dd --version
+fi
+
+. $srcdir/test-lib.sh
+
+p=$abs_top_builddir
+
+fail=0
+
+# Ensure this exits successfully, even though stderr is closed,
+# because it generates no stderr output.
+dd --help >/dev/null 2>&- || fail=1
+
+# If 2>&- works, ensure this fails, because stderr is closed and it
+# *does* generate output.  2>&- apparently does not work in HP-UX 11.23.
+# This test is ineffective unless /dev/stderr also works.
+# This exposes a failure present in 6.11 through 7.5.
+if "$p/src/test" -w /dev/stderr 2>/dev/null &&
+   "$p/src/test" ! -w /dev/stderr 2>&-; then
+  : | dd 2>&- && fail=1
+fi
+
+# Likewise for /dev/full, if /dev/full works.
+if test -w /dev/full && test -c /dev/full; then
+  : | dd 2>/dev/full && fail=1
+fi
+
+Exit $fail

tests/misc/close-stdout
@@ -24,7 +24,7 @@ fi
 
 . $srcdir/test-lib.sh
 
-p="$abs_top_builddir"
+p=$abs_top_builddir
 
 fail=0
 