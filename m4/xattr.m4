m-svo/coreutils

commit 6beca4248f572c2503657fb16341bd43cfa7d254

build: fix build failure due to missing libxattr
Configure is supposed to detect insufficient XATTR support.
However, if a system has the required headers, but no library,
the configure script would mistakenly enable USE_XATTR.
* m4/xattr.m4 (gl_FUNC_XATTR): If the attr_copy_file function
is not found, don't set USE_XATTR.
Nelson Beebe reported a link failure on RHEL 5.3.
Also, do not let the combination of --disable-xattr and
a stray LIB_XATTR environment setting perturb the build.
* NEWS (Build-related): Mention it.

m4/xattr.m4
@@ -15,25 +15,29 @@ AC_DEFUN([gl_FUNC_XATTR],
                        [do not support extended attributes]),
         [use_xattr=$enableval], [use_xattr=yes])
 
+  LIB_XATTR=
+  AC_SUBST([LIB_XATTR])
+
   if test "$use_xattr" = "yes"; then
     AC_CHECK_HEADERS([attr/error_context.h attr/libattr.h])
+    use_xattr=no
     if test $ac_cv_header_attr_libattr_h = yes \
-       && test $ac_cv_header_attr_error_context_h = yes; then
-      use_xattr=1
-    else
-      use_xattr=0
-      AC_MSG_WARN([libattr development library was not found or not usable.])
-      AC_MSG_WARN([AC_PACKAGE_NAME will be built without xattr support.])
+        && test $ac_cv_header_attr_error_context_h = yes; then
+      xattr_saved_LIBS=$LIBS
+      AC_SEARCH_LIBS([attr_copy_file], [attr],
+                     [test "$ac_cv_search_attr_copy_file" = "none required" ||
+                        LIB_XATTR=$ac_cv_search_attr_copy_file])
+      AC_CHECK_FUNCS([attr_copy_file])
+      LIBS=$xattr_saved_LIBS
+      if test $ac_cv_func_attr_copy_file = yes; then
+        use_xattr=yes
+      fi
     fi
     AC_DEFINE_UNQUOTED([USE_XATTR], [$use_xattr],
                        [Define if you want extended attribute support.])
-    LIB_XATTR=
-    xattr_saved_LIBS=$LIBS
-    AC_SEARCH_LIBS([attr_copy_file], [attr],
-                   [test "$ac_cv_search_attr_copy_file" = "none required" ||
-                      LIB_XATTR=$ac_cv_search_attr_copy_file])
-    AC_CHECK_FUNCS([attr_copy_file])
-    LIBS=$xattr_saved_LIBS
-    AC_SUBST([LIB_XATTR])
+    if test $use_xattr = no; then
+      AC_MSG_WARN([libattr development library was not found or not usable.])
+      AC_MSG_WARN([AC_PACKAGE_NAME will be built without xattr support.])
+    fi
   fi
 ])

commit 56fcf8a16b83e6bbba022f41a1c46da5f0b25520

build: fix failure from bogus USE_XATTR definition
* m4/xattr.m4 (gl_FUNC_ADDR): Fix regression introduced in commit
6beca4248.
* THANKS: Update.
Reported by Adam Sampson.

THANKS
@@ -12,6 +12,7 @@ Aaron Hawley                        ashawley@uvm.edu
 Achim Blumensath                    blume@corona.oche.de
 Adam Jimerson                       vendion@charter.net
 Adam Klein                          aklein@debian.org
+Adam Sampson                        ats@offog.org
 Adrian Bunk                         bunk@stusta.de
 AIDA Shinra                         shinra@j10n.org
 Akim Demaille                       demaille@inf.enst.fr

m4/xattr.m4
@@ -1,4 +1,5 @@
 # xattr.m4 - check for Extended Attributes (Linux)
+# serial 3
 
 # Copyright (C) 2003, 2008-2010 Free Software Foundation, Inc.
 # This file is free software; the Free Software Foundation
@@ -33,11 +34,11 @@ AC_DEFUN([gl_FUNC_XATTR],
         use_xattr=yes
       fi
     fi
-    AC_DEFINE_UNQUOTED([USE_XATTR], [$use_xattr],
-                       [Define if you want extended attribute support.])
     if test $use_xattr = no; then
       AC_MSG_WARN([libattr development library was not found or not usable.])
       AC_MSG_WARN([AC_PACKAGE_NAME will be built without xattr support.])
     fi
   fi
+  AC_DEFINE_UNQUOTED([USE_XATTR], [`test $use_xattr != yes; echo $?`],
+                     [Define if you want extended attribute support.])
 ])