m-svo/coreutils

commit 483297d5ddaea930d61da1da9c3f052cbd3c3810

ls --color no longer outputs unnecessary escape sequences
In --color mode, plain files do not get any color, not even white.
When no highlighting is required, ls outputs no escape sequence at all.
* src/ls.c (print_with_color):
(used_color): New global.
(indicator_no) [C_RESET]: New enum value.
(indicator_name) ["rs"]: Corresponding new string.
(color_indicator): Make the 'normal' and 'file' markers be NULL.
Use "rs" (C_RESET) to reset to ordinary colors.
(process_signals): Restore default colors only if necessary.
(main): Don't call prep_non_filename_text here.
(print_name_with_quoting): Call it here, instead.
(prep_non_filename_text): Use C_RESET, not C_NORM.
(print_color_indicator): Return bool, not void.
Print nothing, when possible.
(put_indicator): Call prep_non_filename_text the first time.
* tests/misc/ls-misc: Test for above.
* tests/ls/color-dtype-dir: Adapt: no escapes around regular file name.
* TODO: Remove item.
* NEWS: Mention this.

src/ls.c
@@ -207,7 +207,7 @@ static bool file_ignored (char const *name);
 static uintmax_t gobble_file (char const *name, enum filetype type,
 			      ino_t inode, bool command_line_arg,
 			      char const *dirname);
-static void print_color_indicator (const char *name, mode_t mode, int linkok,
+static bool print_color_indicator (const char *name, mode_t mode, int linkok,
 				   bool stat_ok, enum filetype type);
 static void put_indicator (const struct bin_str *ind);
 static void add_ignore_pattern (const char *pattern);
@@ -488,6 +488,12 @@ ARGMATCH_VERIFY (indicator_style_args, indicator_style_types);
 
 static bool print_with_color;
 
+/* Whether we used any colors in the output so far.  If so, we will
+   need to restore the default color later.  If not, we will need to
+   call prep_non_filename_text before using color for the first time. */
+
+static bool used_color = false;
+
 enum color_type
   {
     color_never,		/* 0: default or --color=never */
@@ -506,14 +512,15 @@ enum Dereference_symlink
 
 enum indicator_no
   {
-    C_LEFT, C_RIGHT, C_END, C_NORM, C_FILE, C_DIR, C_LINK, C_FIFO, C_SOCK,
+    C_LEFT, C_RIGHT, C_END, C_RESET, C_NORM, C_FILE, C_DIR, C_LINK,
+    C_FIFO, C_SOCK,
     C_BLK, C_CHR, C_MISSING, C_ORPHAN, C_EXEC, C_DOOR, C_SETUID, C_SETGID,
     C_STICKY, C_OTHER_WRITABLE, C_STICKY_OTHER_WRITABLE
   };
 
 static const char *const indicator_name[]=
   {
-    "lc", "rc", "ec", "no", "fi", "di", "ln", "pi", "so",
+    "lc", "rc", "ec", "rs", "no", "fi", "di", "ln", "pi", "so",
     "bd", "cd", "mi", "or", "ex", "do", "su", "sg", "st",
     "ow", "tw", NULL
   };
@@ -530,8 +537,9 @@ static struct bin_str color_indicator[] =
     { LEN_STR_PAIR ("\033[") },		/* lc: Left of color sequence */
     { LEN_STR_PAIR ("m") },		/* rc: Right of color sequence */
     { 0, NULL },			/* ec: End color (replaces lc+no+rc) */
-    { LEN_STR_PAIR ("0") },		/* no: Normal */
-    { LEN_STR_PAIR ("0") },		/* fi: File: default */
+    { LEN_STR_PAIR ("0") },		/* rs: Reset to ordinary colors */
+    { 0, NULL },			/* no: Normal */
+    { 0, NULL },			/* fi: File: default */
     { LEN_STR_PAIR ("01;34") },		/* di: Directory: bright blue */
     { LEN_STR_PAIR ("01;36") },		/* ln: Symlink: bright cyan */
     { LEN_STR_PAIR ("33") },		/* pi: Pipe: yellow/brown */
@@ -1071,7 +1079,8 @@ process_signals (void)
       int stops;
       sigset_t oldset;
 
-      restore_default_color ();
+      if (used_color)
+	restore_default_color ();
       fflush (stdout);
 
       sigprocmask (SIG_BLOCK, &caught_signals, &oldset);
@@ -1211,8 +1220,6 @@ main (int argc, char **argv)
 	    }
 #endif
 	}
-
-      prep_non_filename_text ();
     }
 
   if (dereference == DEREF_UNDEFINED)
@@ -1327,7 +1334,8 @@ main (int argc, char **argv)
     {
       int j;
 
-      restore_default_color ();
+      if (used_color)
+	restore_default_color ();
       fflush (stdout);
 
       /* Restore the default signal handling.  */
@@ -3790,8 +3798,9 @@ print_name_with_quoting (const char *p, mode_t mode, int linkok,
 			 bool stat_ok, enum filetype type,
 			 struct obstack *stack)
 {
-  if (print_with_color)
-    print_color_indicator (p, mode, linkok, stat_ok, type);
+  bool used_color_this_time
+    = (print_with_color
+       && print_color_indicator (p, mode, linkok, stat_ok, type));
 
   if (stack)
     PUSH_CURRENT_DIRED_POS (stack);
@@ -3801,7 +3810,7 @@ print_name_with_quoting (const char *p, mode_t mode, int linkok,
   if (stack)
     PUSH_CURRENT_DIRED_POS (stack);
 
-  if (print_with_color)
+  if (used_color_this_time)
     {
       process_signals ();
       prep_non_filename_text ();
@@ -3816,7 +3825,7 @@ prep_non_filename_text (void)
   else
     {
       put_indicator (&color_indicator[C_LEFT]);
-      put_indicator (&color_indicator[C_NORM]);
+      put_indicator (&color_indicator[C_RESET]);
       put_indicator (&color_indicator[C_RIGHT]);
     }
 }
@@ -3891,7 +3900,8 @@ print_type_indicator (bool stat_ok, mode_t mode, enum filetype type)
     DIRED_PUTCHAR (c);
 }
 
-static void
+/* Returns whether any color sequence was printed. */
+static bool
 print_color_indicator (const char *name, mode_t mode, int linkok,
 		       bool stat_ok, enum filetype filetype)
 {
@@ -3968,9 +3978,19 @@ print_color_indicator (const char *name, mode_t mode, int linkok,
 	}
     }
 
-  put_indicator (&color_indicator[C_LEFT]);
-  put_indicator (ext ? &(ext->seq) : &color_indicator[type]);
-  put_indicator (&color_indicator[C_RIGHT]);
+  {
+    const struct bin_str *const s
+      = ext ? &(ext->seq) : &color_indicator[type];
+    if (s->string != NULL)
+      {
+	put_indicator (&color_indicator[C_LEFT]);
+	put_indicator (s);
+	put_indicator (&color_indicator[C_RIGHT]);
+	return true;
+      }
+    else
+      return false;
+  }
 }
 
 /* Output a color indicator (which may contain nulls).  */
@@ -3979,6 +3999,12 @@ put_indicator (const struct bin_str *ind)
 {
   size_t i;
   const char *p;
+
+  if (! used_color)
+    {
+      used_color = true;
+      prep_non_filename_text ();
+    }
 
   p = ind->string;
 

TODO
@@ -133,9 +133,6 @@ Changes expected to go in, someday.
 
   Pending copyright papers:
   ------------------------
-  ls --color: Ed Avis' patch to suppress escape sequences for
-    non-highlighted files
-
   getpwnam from Bruce Korb
 
   pb (progress bar) from Miika Pekkarinen

tests/ls/color-dtype-dir
@@ -4,7 +4,7 @@
 # directories the same as the first one -- but only on a file system
 # with dirent.d_type support.
 
-# Copyright (C) 2006-2007 Free Software Foundation, Inc.
+# Copyright (C) 2006-2008 Free Software Foundation, Inc.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -43,7 +43,7 @@ mv o1 out || fail=1
 cat <<\EOF > exp || fail=1
 ^[[0m^[[01;34md^[[0m$
 ^[[34;42mother-writable^[[0m$
-^[[0mout^[[0m$
+out$
 ^[[37;44msticky^[[0m$
 ^[[m
 EOF

tests/misc/ls-misc
@@ -1,7 +1,7 @@
 #!/bin/sh
 # -*- perl -*-
 
-# Copyright (C) 1998, 2000-2007 Free Software Foundation, Inc.
+# Copyright (C) 1998, 2000-2008 Free Software Foundation, Inc.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -55,8 +55,14 @@ system (qq(touch setuid && chmod u+s setuid && $test -u setuid &&
   or (warn "$program_name: cannot create setuid/setgid/sticky files,"
       . "so can't run this test\n"), exit 77;
 
-my $mkdir = {PRE => sub {mkdir 'd',0755 or die "d: $!\n"}};
-my $rmdir = {POST => sub {rmdir 'd' or die "d: $!\n"}};
+sub mkdir_d {mkdir 'd',0755 or die "d: $!\n"}
+sub rmdir_d {rmdir 'd'      or die "d: $!\n"}
+my $mkdir = {PRE => sub {mkdir_d}};
+my $rmdir = {POST => sub {rmdir_d}};
+my $mkdir_reg = {PRE => sub {mkdir_d; open (FH, '>d/f') && close FH
+			       or die "d/f: $!\n" }};
+my $rmdir_reg = {POST => sub {unlink 'd/f' or die "d/f: $!\n";
+			      rmdir 'd' or die "d: $!\n"}};
 
 my $mkdir2 = {PRE => sub {mkdir 'd',0755 or die "d: $!\n";
 			  mkdir 'd/e',0755 or die "d/e: $!\n" }};
@@ -131,6 +137,11 @@ my @Tests =
      ['sl-dF-color', '-dF --color=always d',
 				 {OUT => "\e[0m\e[01;36md\e[0m\@\n\e[m"},
 				  $slink_d, $unlink_d],
+
+     # A listing with no output should have no color sequences at all.
+     ['no-c-empty', '--color=always d', {OUT => ""}, $mkdir, $rmdir],
+     # A listing with only regular files should have no color sequences at all.
+     ['no-c-reg', '--color=always d', {OUT => "f\n"}, $mkdir_reg, $rmdir_reg],
 
      # Test for a bug fixed after coreutils-6.9.
      ['sl-target', '--color=always d',

commit d2283e50a82c0c3bcf55d8da3fb11dc8598ac93c

ls: fix a regression by honoring NORMAL attributes again
Output the NORMAL attribute before non file name text.
This attribute will continue into file names that would
not otherwise be colored unless FILE is also set.
The regression was introduced with commit 483297d5, 28-02-2009,
"ls --color no longer outputs unnecessary escape sequences".

* src/ls.c (set_normal_color): A new function to output the
NORMAL attribute sequence if it's enabled.
(print_current_files): Output NORMAL before printing long format info.
(print_file_name_and_frills): Output NORMAL before printing file name.
(print_color_indicator): Reset the attributes before a file name with
attributes so that NORMAL attributes will not combine with them.
(print_name_with_quoting): Ensure attributes are reset after printing
the file name if NORMAL attributes were output.
* tests/ls/color-norm: A new test for NORMAL and FILE combinations.
* tests/Makefile.am: Reference the new test.
* NEWS: Mention the fix.
Reported in https://savannah.gnu.org/bugs/?26512

src/ls.c
@@ -1149,6 +1149,17 @@ restore_default_color (void)
   put_indicator (&color_indicator[C_RIGHT]);
 }
 
+static void
+set_normal_color (void)
+{
+  if (print_with_color && is_colored (C_NORM))
+    {
+      put_indicator (&color_indicator[C_LEFT]);
+      put_indicator (&color_indicator[C_NORM]);
+      put_indicator (&color_indicator[C_RIGHT]);
+    }
+}
+
 /* An ordinary signal was received; arrange for the program to exit.  */
 
 static void
@@ -3444,6 +3455,7 @@ print_current_files (void)
     case long_format:
       for (i = 0; i < cwd_n_used; i++)
         {
+          set_normal_color ();
           print_long_format (sorted_file[i]);
           DIRED_PUTCHAR ('\n');
         }
@@ -4004,7 +4016,9 @@ print_name_with_quoting (const struct fileinfo *f,
   const char* name = symlink_target ? f->linkname : f->name;
 
   bool used_color_this_time
-    = (print_with_color && print_color_indicator (f, symlink_target));
+    = (print_with_color
+        && (print_color_indicator (f, symlink_target)
+            || is_colored (C_NORM)));
 
   if (stack)
     PUSH_CURRENT_DIRED_POS (stack);
@@ -4047,6 +4061,8 @@ static size_t
 print_file_name_and_frills (const struct fileinfo *f, size_t start_col)
 {
   char buf[MAX (LONGEST_HUMAN_READABLE + 1, INT_BUFSIZE_BOUND (uintmax_t))];
+
+  set_normal_color ();
 
   if (print_inode)
     printf ("%*s ", format == with_commas ? 0 : inode_number_width,
@@ -4218,6 +4234,9 @@ print_color_indicator (const struct fileinfo *f, bool symlink_target)
       = ext ? &(ext->seq) : &color_indicator[type];
     if (s->string != NULL)
       {
+        /* Need to reset so not dealing with attribute combinations */
+        if (is_colored (C_NORM))
+          restore_default_color ();
         put_indicator (&color_indicator[C_LEFT]);
         put_indicator (s);
         put_indicator (&color_indicator[C_RIGHT]);

tests/Makefile.am
@@ -370,6 +370,7 @@ TESTS =						\
   ls/abmon-align				\
   ls/color-clear-to-eol				\
   ls/color-dtype-dir				\
+  ls/color-norm					\
   ls/dangle					\
   ls/dired					\
   ls/file-type					\

tests/ls/color-norm
@@ -1,0 +1,80 @@
+#!/bin/sh
+# Ensure "ls --color" properly colors "normal" text and files.
+# I.E. that it uses NORMAL to style non file name output and
+# file names with no associated color (unless FILE is also set).
+
+# Copyright (C) 2010 Free Software Foundation, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if test "$VERBOSE" = yes; then
+  set -x
+  ls --version
+fi
+
+. $srcdir/test-lib.sh
+
+# Don't let a different umask perturb the results.
+umask 22
+
+touch exe || framework_failure
+chmod u+x exe || framework_failure
+touch nocolor || framework_failure
+
+# Non coloured files inherit NORMAL attributes
+LS_COLORS=no=7      ls -s1U --color exe nocolor >> out || fail=1
+LS_COLORS=no=7      ls -sxU --color exe nocolor >> out || fail=1
+LS_COLORS=no=7      ls -s1U --color nocolor exe >> out || fail=1
+LS_COLORS=no=7      ls -sxU --color nocolor exe >> out || fail=1
+
+# NORMAL does not override FILE though
+LS_COLORS=no=7:fi=1 ls -s1U --color nocolor exe >> out || fail=1
+
+# Support uncolored ordinary files that do _not_ inherit from NORMAL.
+# Note there is a redundant RESET output before a non colored
+# file in this case which may be removed in future.
+LS_COLORS=no=7:fi=  ls -s1U --color nocolor exe >> out || fail=1
+LS_COLORS=no=7:fi=0 ls -s1U --color nocolor exe >> out || fail=1
+
+# A caveat worth noting is that commas (-m), indicator chars (-F)
+# and the "total" line, do not currently use NORMAL attributes
+LS_COLORS="no=7"    ls -mFU --color nocolor exe >> out || fail=1
+
+# Ensure no coloring is done unless enabled
+LS_COLORS="no=7"    ls -s1U         nocolor exe >> out || fail=1
+
+cat -A out > out.display || framework_failure
+mv out.display out || framework_failure
+
+cat <<\EOF > exp || framework_failure
+^[[0m^[[7m0 ^[[m^[[01;32mexe^[[0m$
+^[[7m0 nocolor^[[0m$
+^[[0m^[[7m0 ^[[m^[[01;32mexe^[[0m  ^[[7m0 nocolor^[[0m$
+^[[0m^[[7m0 nocolor^[[0m$
+^[[7m0 ^[[m^[[01;32mexe^[[0m$
+^[[0m^[[7m0 nocolor^[[0m  ^[[7m0 ^[[m^[[01;32mexe^[[0m$
+^[[0m^[[7m0 ^[[m^[[1mnocolor^[[0m$
+^[[7m0 ^[[m^[[01;32mexe^[[0m$
+^[[0m^[[7m0 ^[[m^[[mnocolor^[[0m$
+^[[7m0 ^[[m^[[01;32mexe^[[0m$
+^[[0m^[[7m0 ^[[m^[[0mnocolor^[[0m$
+^[[7m0 ^[[m^[[01;32mexe^[[0m$
+^[[0m^[[7mnocolor^[[0m, ^[[7m^[[m^[[01;32mexe^[[0m*$
+0 nocolor$
+0 exe$
+EOF
+
+compare out exp || fail=1
+
+Exit $fail