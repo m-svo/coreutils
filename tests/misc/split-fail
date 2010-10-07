m-svo/coreutils

commit 23f6d41f94302a8a859f8e54ece2cc0b9e8dfb83

Include "full-read.h". (bytes_split, lines_split, line_bytes_split): Use full_read, not safe_read.   The way split was using the latter, a short read could cause split to terminate before EOF.
(bytes_split): Remove unnecessary `else' after break.
(lines_split): Likewise.  and correct misleading indentation.

src/split.c
@@ -1,5 +1,5 @@
 /* split.c -- split a file into pieces.
-   Copyright (C) 88, 91, 1995-2002 Free Software Foundation, Inc.
+   Copyright (C) 88, 91, 1995-2003 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -31,6 +31,7 @@
 #include "closeout.h"
 #include "dirname.h"
 #include "error.h"
+#include "full-read.h"
 #include "full-write.h"
 #include "posixver.h"
 #include "safe-read.h"
@@ -209,7 +210,7 @@ bytes_split (size_t nchars, char *buf, size_t bufsize)
 
   do
     {
-      n_read = safe_read (input_desc, buf, bufsize);
+      n_read = full_read (input_desc, buf, bufsize);
       if (n_read == SAFE_READ_ERROR)
         error (EXIT_FAILURE, errno, "%s", infile);
       bp_out = buf;
@@ -226,14 +227,12 @@ bytes_split (size_t nchars, char *buf, size_t bufsize)
 		}
 	      break;
 	    }
-	  else
-	    {
-	      cwrite (new_file_flag, bp_out, to_write);
-	      bp_out += to_write;
-	      to_read -= to_write;
-	      new_file_flag = 1;
-	      to_write = nchars;
-	    }
+
+	  cwrite (new_file_flag, bp_out, to_write);
+	  bp_out += to_write;
+	  to_read -= to_write;
+	  new_file_flag = 1;
+	  to_write = nchars;
 	}
     }
   while (n_read == bufsize);
@@ -252,7 +251,7 @@ lines_split (size_t nlines, char *buf, size_t bufsize)
 
   do
     {
-      n_read = safe_read (input_desc, buf, bufsize);
+      n_read = full_read (input_desc, buf, bufsize);
       if (n_read == SAFE_READ_ERROR)
 	error (EXIT_FAILURE, errno, "%s", infile);
       bp = bp_out = buf;
@@ -271,15 +270,15 @@ lines_split (size_t nlines, char *buf, size_t bufsize)
 		}
 	      break;
 	    }
-	  else
-	    ++bp;
-	    if (++n >= nlines)
-	      {
-		cwrite (new_file_flag, bp_out, bp - bp_out);
-		bp_out = bp;
-		new_file_flag = 1;
-		n = 0;
-	      }
+
+	  ++bp;
+	  if (++n >= nlines)
+	    {
+	      cwrite (new_file_flag, bp_out, bp - bp_out);
+	      bp_out = bp;
+	      new_file_flag = 1;
+	      n = 0;
+	    }
 	}
     }
   while (n_read == bufsize);
@@ -302,7 +301,7 @@ line_bytes_split (size_t nchars)
     {
       /* Fill up the full buffer size from the input file.  */
 
-      n_read = safe_read (input_desc, buf + n_buffered, nchars - n_buffered);
+      n_read = full_read (input_desc, buf + n_buffered, nchars - n_buffered);
       if (n_read == SAFE_READ_ERROR)
 	error (EXIT_FAILURE, errno, "%s", infile);
 

commit 06d3d78c902ae94d67c837d05726adabbf143d37

split: fix reporting of read errors
The bug was introduced with commit 23f6d41f, 19-02-2003.

* src/split.c (bytes_split, lines_split, line_bytes_split):
Correctly check the return from full_read().
* tests/misc/split-fail: Ensure split fails when
it can't read its input.
* NEWS: Mention the fix.

src/split.c
@@ -33,7 +33,6 @@
 #include "full-read.h"
 #include "full-write.h"
 #include "quote.h"
-#include "safe-read.h"
 #include "xfreopen.h"
 #include "xstrtol.h"
 
@@ -229,7 +228,7 @@ bytes_split (uintmax_t n_bytes, char *buf, size_t bufsize)
   do
     {
       n_read = full_read (STDIN_FILENO, buf, bufsize);
-      if (n_read == SAFE_READ_ERROR)
+      if (n_read < bufsize && errno)
         error (EXIT_FAILURE, errno, "%s", infile);
       bp_out = buf;
       to_read = n_read;
@@ -273,7 +272,7 @@ lines_split (uintmax_t n_lines, char *buf, size_t bufsize)
   do
     {
       n_read = full_read (STDIN_FILENO, buf, bufsize);
-      if (n_read == SAFE_READ_ERROR)
+      if (n_read < bufsize && errno)
         error (EXIT_FAILURE, errno, "%s", infile);
       bp = bp_out = buf;
       eob = bp + n_read;
@@ -314,7 +313,6 @@ lines_split (uintmax_t n_lines, char *buf, size_t bufsize)
 static void
 line_bytes_split (size_t n_bytes)
 {
-  size_t n_read;
   char *bp;
   bool eof = false;
   size_t n_buffered = 0;
@@ -324,8 +322,9 @@ line_bytes_split (size_t n_bytes)
     {
       /* Fill up the full buffer size from the input file.  */
 
-      n_read = full_read (STDIN_FILENO, buf + n_buffered, n_bytes - n_buffered);
-      if (n_read == SAFE_READ_ERROR)
+      size_t to_read = n_bytes - n_buffered;
+      size_t n_read = full_read (STDIN_FILENO, buf + n_buffered, to_read);
+      if (n_read < to_read && errno)
         error (EXIT_FAILURE, errno, "%s", infile);
 
       n_buffered += n_read;

tests/misc/split-fail
@@ -64,5 +64,11 @@ split: line count option -99*... is too large
 EOF
 compare out exp || fail=1
 
+# Make sure split fails when it can't read input
+# (the current directory in this case)
+if ! cat . >/dev/null; then
+  # can't read() directories
+  split . && fail=1
+fi
 
 Exit $fail